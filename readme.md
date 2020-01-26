fbeba36c679f80cdbc57e0fc1e29f567c9257555
Guillaume Lhermenier <guillaume.lhermenier@gmail.com>
2020-01-26 22:48:23 +0100

    add readme & format code

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Rust crawler to gather links list from a webpage
+
+## Compilation
+Just launch `cargo fmt --all -- --check && cargo clippy && cargo build --release --bin rust-crawler` so that all your code will be linted and checked.  
+*NB*:Rust `1.36` or newer is needed.  
+
+## Usage
+Link to be crawled is hardcoded for now, so just launch the binary you built.
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 extern crate url_crawler;
-use url_crawler::*;
+
 use std::sync::Arc;
- 
+use url_crawler::*;
+
 /// Function for filtering content in the crawler before a HEAD request.
-/// 
 /// Only allow directory entries, and files that have the `deb` extension.
+
 fn apt_filter(url: &Url) -> bool {
     let url = url.as_str();
     url.ends_with("/") || url.ends_with(".deb")
 }
- 
 pub fn main() {
     // Create a crawler designed to crawl the given website.
     let crawler = Crawler::new("https://training.engineering.publicissapient.fr/".to_owned())
@@ -19,7 +19,6 @@ pub fn main() {
         .pre_fetch(Arc::new(apt_filter))
         // Initialize the crawler and begin crawling. This returns immediately.
         .crawl();
- 
     // Process url entries as they become available
     for file in crawler {
         println!("{:#?}", file);
